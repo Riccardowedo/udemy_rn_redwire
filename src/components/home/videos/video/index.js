ee1421433ac43702f1650c24e4dba785828dc0d6
devdummy <dev.teach.user.dummy@gmail.com>
2020-10-24 07:03:41 -0300

    Adding youtube videos

--- src/components/home/videos/video/index.js
+++ src/components/home/videos/video/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { View, Text, ScrollView } from 'react-native';
 import ContentShow from '../../../../utils/contentShow';
+import Youtube from 'react-native-youtube';
 
 const VideoScreen = () => {
     return(
         <ScrollView>
             <View>
+                <Youtube
+                    apiKey="AIzaSyCCn-f3qw2j65uvqb9bcxg2T-26XpDWq70"
+                    videoId="Zfsg3oiPXGc"
+                    play={false}
+
+                    onReady={ e => console.log('ready')}
+                    onChangeState={ e => console.log(e)}
+                    onError={error => console.log(error)}
+
+                    style={{alignSelf:'stretch',height:300}}
+                />
                 <ContentShow/>     
             </View>
         </ScrollView>
